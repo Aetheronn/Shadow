e257d28d7cea7d4e4540b4595ca04fc90f9362c0
Aetheronn <125783573+Aetheronn@users.noreply.github.com>
2023-03-16 08:18:33 +0530

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Axiom v1.7 is currently down, Please be patient as we release Axiom v.1.8, JK, V2,\nMeanwhile Roville Devs : Axiom is down now it's gonna be a peaceful life for us, Aetheron: You got the wrong number")
+game.Players.LocalPlayer:Kick("Axiom v1.7 is currently down, Please be patient as we release Axiom v.1.8, JK, V2")
